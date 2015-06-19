9d5a25dce6ff4887bd9eb8770cb144c6d1570cc7
lutao <DwyaneTalk@163.com>
2015-06-19 11:49:41 +0800

    完成二叉树一部分功能

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -9,9 +9,11 @@ BinaryTree::~BinaryTree() {
 
 void BinaryTree::init() {
     if (root) {
-        BinaryTree *leftTree = getLeftChildTree(), *rightTree = getRightChildTree();
+        BinaryTree *leftTree = getChildTree(LEFT), *rightTree = getChildTree(RIGHT);
         leftTree->init();
         rightTree->init();
+        delete leftTree;
+        delete rightTree;
         delete root;
     }
 }
@@ -29,71 +31,97 @@ void BinaryTree::postinCreateBinaryTree(BiTreeElemType *post_data, BiTreeElemTyp
 }
 
 bool BinaryTree::isEmpty() {
+    return !root;
+}
+
+void BinaryTree::getNodeInfo(int& depth, int& allNodeNums, int& leafNodeNums) {
 
 }
 
-int BinaryTree::getDepth() {
-
+BiNode* BinaryTree::getRoot() {
+    return root;
 }
 
-BiNode *getRoot() {
-
-}
-
-int BinaryTree::getAllNodeNums() {
-
-}
-
-int BinaryTree::getLeafNodeNums() {
-
-}
-
-BiNode* findNode(BiTreeElemType data) {
-
+BiNode* BinaryTree::findNode(BiTreeElemType data) {
+    if (root) {
+        if (root->data == data) {
+            return root;
+        }
+        BiNode *ansNode;
+        BinaryTree *leftree = getChildTree(LEFT);
+        ansNode = leftree->findNode(data);
+        delete leftree;
+        if (!ansNode) {
+            BinaryTree *rightTree = getChildTree(RIGHT);
+            ansNode = rightTree->findNode(data);
+            delete rightTree;
+        }
+        return ansNode;
+    }
+   return NULL;
 }
 
 BiTreeElemType BinaryTree::getNodeData(BiNode *node) {
-
+    return node->data;
 }
 
 void BinaryTree::setNodeData(BiNode *node, BiTreeElemType data) {
 
 }
 
-BinaryTree* BinaryTree::getLeftChildTree() {
-
+BinaryTree* BinaryTree::getChildTree( LR lr) {
+    if (root) {
+        BinaryTree *childTree = new BinaryTree;
+        if (lr == LEFT)     childTree->root = root->lchild;
+        else                childTree->root = root->rchild;
+        return childTree;
+    }
+    return NULL;
 }
 
-BinaryTree* BinaryTree::getRightChildTree() {
-
-}
 
 BiNode* BinaryTree::getNodeParent(BiNode *node) {
-
+    if (node)   return node->parent;
+    else        return NULL;
 }
 
-BiNode* BinaryTree::getNodeLChild(BiNode *node) {
-
+BiNode* BinaryTree::getNodeChild(BiNode *node, LR lr) {
+    if (node) {
+       if(lr == LEFT)   return node->lchild;
+       else             return node->rchild;
+    }
+    else        return NULL;
 }
 
-BiNode* BinaryTree::getNodeRChild(BiNode *node) {
 
+BiNode* BinaryTree::getNodeSibling(BiNode *node, LR &lr) {
+    if (node && node->parent) {
+        if (node->parent->lchild == node) {
+            lr = LEFT;
+            return node->parent->rchild;
+        } else {
+            lr = RIGHT;
+            return node->parent->lchild;
+        }
+    }
+    return NULL;
 }
 
-BiNode* BinaryTree::getNodeLSibling(BiNode *node) {
-
-}
-
-BiNode* BinaryTree::getNodeRSibling(BiNode *node) {
-
-}
 
 void BinaryTree::insertChildNode(BiNode *node, LR lr, BiTreeElemType data){
 
 }
 
 BiTreeElemType BinaryTree::deleteChildNode(BiNode *node, LR lr) {
-
+    BiTreeElemType ans = 0;
+    if (node) {
+        BinaryTree *nodeTree = new BinaryTree;
+        nodeTree->root = node;
+        BinaryTree *childTree = nodeTree->getChildTree(lr);
+        ans = getNodeData(getNodeChild(node, lr));
+        if (childTree)  childTree->init();
+    }
+    return ans;
 }
 
 void BinaryTree::preOrderTraverse(void(*visit)(BiTreeElemType &e), BiTreeElemType *data) {
