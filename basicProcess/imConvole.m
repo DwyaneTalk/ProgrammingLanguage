25cad5e7b3e070ccafbe042deecf4a0b9bc07367
lutao <DwyaneTalk@163.com>
2015-08-31 22:28:24 +0800

    完成刚刚没有提交成功的修改

--- basicProcess/imConvole.m
+++ basicProcess/imConvole.m
@@ -1,8 +1,9 @@
 function [im_out] = imConvole(im_in, template)
-    [row, col] = size(template);
-    im_out = im_in;
-    pad_x = floor(row / 2);
-    pad_y = floor(col / 2);
+    [row, col] = size(im_in);
+    im_out = uint8(zeros(row, col));
+    [trow, tcol] = size(template);
+    pad_x = floor(trow / 2);
+    pad_y = floor(tcol / 2);
     for x = pad_x + 1 : row - pad_x
         for y = pad_y + 1 : col - pad_y
             im_out(x, y) = sum(sum(double(im_in(x - pad_x : x + pad_x, y - pad_y : y + pad_y)) .* template));
--- morphology/morphology.m
+++ morphology/morphology.m
@@ -1,2 +1,68 @@
 function morphology()
+    im = imread('lena_gray.jpg');
+    im = threshold(im);
+    template = ones(3) * 255;
+    subplot(2,2,1);
+    imshow(im);
+    title('Ô­Ê¼Í¼Ïñ');
+    subplot(2,2,2);
+    im_erosion = erosion_binary(im, template);
+    imshow(im_erosion);
+    title('¸¯Ê´Í¼Ïñ');
+    subplot(2,2,3);
+    template = zeros(3, 3);
+    im_dilation = dilation_binary(im, template);
+    imshow(im_dilation);
+    title('ÅòÕÍÍ¼Ïñ');
+    subplot(2,2,4);
+    imshow(im_dilation - im_erosion);
+    title('²îÖµÍ¼Ïñ');
+end
+
+%% refer:http://blog.sina.com.cn/s/blog_6b205d630100sgk1.html
+
+% ¸¯Ê´
+function [im_out] = erosion_binary(im_in, template)
+    if(find(im_in(im_in > 0) < 255))
+        disp('´íÎó£¡·Ç¶þÖµÍ¼Ïñ');
+    end
+    [row, col] = size(im_in);
+    [trow, tcol] = size(template);
+    template = uint8(template);
+    rhal = floor(trow / 2);
+    chal = floor(tcol / 2);
+    im_out = uint8(zeros(row - 2 * rhal, col - 2 * chal));
+    for irow = 1 + rhal : row - rhal
+        for icol = 1 + chal : col - chal
+            im_diff = im_in(irow - rhal : irow + rhal, icol - chal : icol + chal) ~= template;
+            if(any(im_diff(:)))
+                im_out(irow, icol) = 0;
+            else
+                im_out(irow, icol) = 255;
+            end
+        end
+    end
+end
+
+% ÅòÕÍ
+function [im_out] = dilation_binary(im_in,template)
+    if(find(im_in(im_in > 0) < 255))
+        disp('´íÎó£¡·Ç¶þÖµÍ¼Ïñ');
+    end
+    [row, col] = size(im_in);
+    [trow, tcol] = size(template);
+    template = uint8(template);
+    rhal = floor(trow / 2);
+    chal = floor(tcol / 2);
+    im_out = uint8(zeros(row - 2 * rhal, col - 2 * chal));
+    for irow = 1 + rhal : row - rhal
+        for icol = 1 + chal : col - chal
+            im_diff = im_in(irow - rhal : irow + rhal, icol - chal : icol + chal) ~= template;
+            if(any(im_diff(:)))
+                im_out(irow, icol) = 255;
+            else
+                im_out(irow, icol) = 0;
+            end
+        end
+    end
 end
